15b165e703c3bfe0c3bd3ac8e23409ac8600dd3a
sonu-shivcharan <sonushivcharan@outlook.com>
2026-02-01 13:55:53 +0530

    correct api url

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,4 +1,9 @@
-export const USER_API_ENDPOINT = "https://job-portal-deploy.onrender.com/api/user";
-export const JOB_API_ENDPOINT = "https://job-portal-deploy.onrender.com/api/job";
-export const APPLICATION_API_ENDPOINT = "https://job-portal-deploy.onrender.com/api/application";
-export const COMPANY_API_ENDPOINT = "https://job-portal-deploy.onrender.com/api/company";
+const API_URL = import.meta.env.VITE_API_URL;
+if (!API_URL) {
+  console.error("API URL is not defined");
+  throw new Error("API URL is not defined");
+}
+export const USER_API_ENDPOINT = `${API_URL}/api/user`;
+export const JOB_API_ENDPOINT = `${API_URL}/api/job`;
+export const APPLICATION_API_ENDPOINT = `${API_URL}/api/application`;
+export const COMPANY_API_ENDPOINT = `${API_URL}/api/company`;
